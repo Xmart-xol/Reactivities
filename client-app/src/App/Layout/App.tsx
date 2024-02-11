0197224bbc94e357ba39596d7fc47a07fd13f50e
Xmart-xol <kulagiven@gmail.com>
2024-02-11 05:56:22 +0200

    view, edita and add activities UI

---
+++ client-app/src/App/Layout/App.tsx
@@ -0,0 +1,58 @@
+import { Fragment, useEffect, useState } from "react"
+import axios from 'axios'
+import { Container, Header, List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
+import "./styles.css";
+import ActivityDashboard from "../../Features/activities/dashboard/ActivityDashboard";
+
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
+  
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data)
+      })
+  }, [])
+
+  function handleSelectActivity(id: string){
+    setSelectedActivity(activities.find(x =>x.id ===id))
+  }
+
+  function handleCancelSelectedActivity(){
+    setSelectedActivity(undefined)
+  }
+
+  function handleFormOpen(id?: string){
+    id ? handleSelectActivity(id) : handleCancelSelectedActivity();
+    setEditMode(true);
+  }
+
+
+  function handleFormClose(){
+    setEditMode(false);
+  }
+
+  return (
+      <>
+       <NavBar openForm={handleFormOpen}/>
+       <Container style={{marginTop: '7em'}}>
+          <ActivityDashboard 
+          activities ={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectedActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          />
+       </Container>
+      </>
+  );
+}
+
+export default App
