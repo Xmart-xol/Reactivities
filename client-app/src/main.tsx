ec2d49bc50753d363307794f464c345069c081c9
Xmart-xol <kulagiven@gmail.com>
2024-03-16 07:37:56 +0200

    update strict mode

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -7,10 +7,8 @@ import { StoreContext, store } from './App/stores/store'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <StoreContext.Provider value={store}>
        <App />
     </StoreContext.Provider>
-   
-  </React.StrictMode>,
+  
 )
